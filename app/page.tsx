e34dfa15133245500f7a7a30ceeddbe73eb2f6be
Ignacio Rodriguez Rulas <ignaciofabian93@gmail.com>
2024-09-06 06:16:30 -0400

    no eslint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { Spinner } from "@nextui-org/spinner";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { GetMe } from "@/services/auth";
 import useSessionStore from "@/store/session";
 
@@ -9,33 +9,36 @@ export default function Auth() {
   const router = useRouter();
   const { session, setSession } = useSessionStore();
 
-  const redirect = (path: string) => {
-    setTimeout(() => {
-      router.push(path);
-    }, 2000);
-  };
+  const redirect = useCallback(
+    (path: string) => {
+      setTimeout(() => {
+        router.push(path);
+      }, 2000);
+    },
+    [router]
+  );
 
   useEffect(() => {
+    const getUserInfo = async () => {
+      const response = await GetMe({ token: session.token });
+      if (response.user) {
+        const newSession = {
+          ...session,
+          id: response.user.id,
+          email: response.user.email,
+          name: response.user.name,
+        };
+        setSession(newSession);
+        redirect("/home");
+      }
+    };
+
     if (session.token) {
       getUserInfo();
     } else {
       redirect("/login");
     }
-  }, [session]);
-
-  const getUserInfo = async () => {
-    const response = await GetMe({ token: session.token });
-    if (response.user) {
-      const newSession = {
-        ...session,
-        id: response.user.id,
-        email: response.user.email,
-        name: response.user.name,
-      };
-      setSession(newSession);
-      redirect("/home");
-    }
-  };
+  }, [session, setSession, redirect]);
 
   return (
     <main className="w-screen h-screen">
